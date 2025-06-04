0f8b1e51eb8d8f5dd00fcd5bf16949fa782c64b7
lorenzo giduci <lorenzo0547@gmail.com>
2025-06-04 21:25:23 +0200

    .

--- script.js
+++ script.js
@@ -3,11 +3,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const img = urlParams.get('img');
     const imgElement = document.getElementById("immagine");
 
+    // Redirect to index3.html if ?img=3 on index.html
+    if (
+        window.location.pathname.endsWith('index.html') &&
+        img === '3'
+    ) {
+        window.location.replace('index3.html');
+        return;
+    }
+
+    // Redirect to index4.html if ?img=2 on index.html
+    if (
+        window.location.pathname.endsWith('index.html') &&
+        img === '2'
+    ) {
+        window.location.replace('index4.html');
+        return;
+    }
+
     const imgMap = {
-        "1": { src: "https://media.giphy.com/media/xT9IgG50Fb7Mi0prBC/giphy.gif", alt: "Gif animata droga", title: "Dipendenza da droga" },
-        "2": { src: "https://media.giphy.com/media/l0HlOvJ7yaacpuSas/giphy.gif", alt: "Gif animata gioco", title: "Dipendenza da gioco" },
-        "3": { src: "https://media.giphy.com/media/26AHONQ79FdWZhAI0/giphy.gif", alt: "Gif animata fumo", title: "Dipendenza da fumo" },
-        "4": { src: "https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif", alt: "Gif animata sesso", title: "Dipendenza da sesso" }
+        "1": { src: "immagini/Happy Season 5 GIF by The Office.gif", alt: "Gif animata droga", title: "this u" },
+        "2": { src: "immagini/You Bro New York Jets GIF by Nickelodeon at Super Bowl.gif", alt: "Gif animata gioco", title: "this u" },
+        "3": { src: "immagini/Happy Lionel Messi GIF.gif", alt: "Gif animata fumo", title: "this u" },
+        "4": { src: "immagini/I Am Coming For You Oh No GIF by Manifest Destiny Down SPACETIME.gif", alt: "Gif animata sesso", title: "this u" }
     };
 
     // Gestione immagine dinamica SOLO su index.html (o index1)
@@ -25,8 +43,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.title = imgMap[img].title;
             }
         } else {
-            imgElement.src = "immagini/assefazione.jpg";
-            imgElement.alt = "Immagine di assuefazione";
+            imgElement.src = "immagini/Chuck Liddell Point GIF by UFC.gif";
+            imgElement.alt = "This u";
             if (document.title !== "this u") {
                 document.title = "this u";
             }
